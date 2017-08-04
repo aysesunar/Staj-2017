c428c987ef0c9f21a3c24ae817b224488d5efce6
Emine Ayse Sunar <Emineayse@outlook.com>
2017-08-04 11:03:10 +0300

    3rd commit

--- staj.c
+++ staj.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello there!\nOh and also welcome to github!");
+	printf("Hello there!\nOh and also welcome to github!\n"
+			"making some changes");
 	return 0;
 }
